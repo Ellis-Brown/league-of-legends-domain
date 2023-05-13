f9a68e490e07e93a8cc68a468c17d16ddbfe5799
Ellis Brown <ellis.brown@tufts.edu>
2023-05-13 00:28:23 -0400

    Add animation

---
+++ src/components/MovingText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import '../styles/global.css';
+
+const MovingText = () => {
+  return (
+    <div className="container">
+      <span className="moving-text">
+        Wow so cool!!!</span>
+    </div>
+  );
+};
+
+export default MovingText;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
+import MovingText from '../components/MovingText';
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -18,14 +19,20 @@ export default function Home() {
       </Head>
 
       <main className={`h-screen w-full ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'} transition-colors duration-300`}>
-        <div className="h-screen bg-gradient-to-r from-blue-500 via-pink-600 to-amber-400 animate-gradient-x w-full flex flex-col items-center justify-center">
+        <div>
+        <MovingText />
+        <div className="h-screen bg-gradient-to-r from-amber-400 via-pink-600 to-blue-500 animate-gradient-x w-full flex flex-col items-center justify-center">
           <h1 className="text-6xl md:text-8xl lg:text-9xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-600 p-3">
             League of Legends
           </h1>
+          
           <h2 className={`mt-9 ${isDarkMode ? 'text-gray-200' : 'text-gray-800'} text-2xl font-semibold`}>
             The best three words in a sequence ever! 🤩
           </h2>
+          
+          </div>
         </div>
+        
       </main>
     </>
   );
